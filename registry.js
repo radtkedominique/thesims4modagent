47262b2e6b546c81330b12fb33c7de2ecb776b4f
Dominique Radtke <dominique.radtke@haufe-lexware.com>
2026-07-22 20:39:56 +0200

    blanko registry

---
+++ registry.js
@@ -0,0 +1,28 @@
+{
+  "registry_version": 1,
+  "last_updated": "2026-07-22",
+  "mods": [
+    {
+      "name": "WickedWhims",
+      "source": "https://wickedwhimsmod.com/download",
+      "tier": "check",
+      "installed_version": "178",
+      "files": ["WickedWhims.ts4script", "WickedWhims.package"],
+      "version_hint": "",
+      "download_hint": "",
+      "last_verified": null,
+      "notes": ""
+    },
+    {
+      "name": "",
+      "source": "unknown",
+      "tier": "check",
+      "installed_version": "",
+      "files": [],
+      "version_hint": "",
+      "download_hint": "",
+      "last_verified": null,
+      "notes": ""
+    }
+  ]
+}
